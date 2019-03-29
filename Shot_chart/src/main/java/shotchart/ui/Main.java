fd7841888aa270771c6fc3ee1200265bc19d7c62
Niko Häppölä <niko.happola@gmail.com>
2019-03-29 12:05:07 +0200

    Maven-plugineita kuntoon.

---
+++ Shot_chart/src/main/java/shotchart/ui/Main.java
@@ -0,0 +1,11 @@
+package shotchart.ui;
+
+// @deemus
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello shooting map!");
+    
+    }
+}
---
+++ Shot_chart/src/main/java/shotchart/ui/UserInterface.java
@@ -0,0 +1,20 @@
+package shotchart.ui;
+
+// @deemus
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class UserInterface extends Application {
+
+    
+    
+    
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
