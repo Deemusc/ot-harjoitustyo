621c36ec7d4209ce8a2a12aff35a8fff9c0f348f
Niko Häppölä <niko.happola@gmail.com>
2019-05-04 18:03:48 +0300

    JavaDociin pieniä lisäyksiä.

--- Shot_chart/src/main/java/shotchart/dao/ShotChartDao.java
+++ Shot_chart/src/main/java/shotchart/dao/ShotChartDao.java
@@ -1,20 +1,23 @@
 package shotchart.dao;
 
 // @deemus
-
+/**
+ * Rajapinta, jonka avulla huolehditaan laukaisukarttojen
+ * pysyväistallennuksesta.
+ */
 import java.util.ArrayList;
 import shotchart.domain.ShotChart;
 
-
 public interface ShotChartDao {
+
     ShotChart create(ShotChart shotChart) throws Exception;
 
     ShotChart update(ShotChart shotChart) throws Exception;
-    
+
     void delete(ShotChart shotChart) throws Exception;
-    
+
     ShotChart getChart(int id) throws Exception;
-    
+
     ArrayList<ShotChart> getAll();
-        
+
 }
--- Shot_chart/src/main/java/shotchart/dao/UserDao.java
+++ Shot_chart/src/main/java/shotchart/dao/UserDao.java
@@ -1,17 +1,18 @@
 package shotchart.dao;
 
 // @deemus
-
+/**
+ * Rajapinta, jonka avulla huolehditaan käyttäjien pysyväistallennuksesta.
+ */
 import java.util.List;
 import shotchart.domain.User;
 
-
 public interface UserDao {
-    
+
     User create(User user) throws Exception;
-    
+
     User findByUsername(String username);
-    
+
     List<User> getAll();
-    
+
 }
